9f650d8eae33a4afcfb096c9d568d0050f9270a9
Dominic Rufa <46943710+dominicrufa@users.noreply.github.com>
2024-04-11 19:23:13 -0400

    disallow openmm system querying for improper torsions; throws duplication errors in building intermediate rbfe states

--- timemachine/ff/handlers/bonded.py
+++ timemachine/ff/handlers/bonded.py
@@ -316,50 +316,46 @@ class ImproperTorsionHandler(SerializableMixIn):
 
     @staticmethod
     def static_parameterize(params, smirks, mol):
-        # first query omm system:
-        if hasattr(mol, 'openmm_system'):
-            assigned_params, improper_idxs = handle_omm_torsions(mol, proper = False)
+        # improper torsions do not use a valence dict as
+        # we cannot sort based on b_idxs[0] and b_idxs[-1]
+        # and reverse if needed. Impropers are centered around
+        # the first atom.
+        impropers = dict()
+
+        def make_key(idxs):
+            assert len(idxs) == 4
+            # pivot around the center
+            ctr = idxs[1]
+            # sort the neighbors so they're unique
+            nbs = idxs[0], idxs[2], idxs[3]
+            nbs = sorted(nbs)
+            return nbs[0], ctr, nbs[1], nbs[2]
+
+        for p_idx, patt in enumerate(smirks):
+            matches = match_smirks(mol, patt)
+
+            for m in matches:
+                key = make_key(m)
+                impropers[key] = p_idx
+
+        improper_idxs = []
+        param_idxs = []
+
+        for atom_idxs, p_idx in impropers.items():
+            center = atom_idxs[1]
+            others = [atom_idxs[0], atom_idxs[2], atom_idxs[3]]
+            for p in [(others[i], others[j], others[k]) for (i, j, k) in [(0, 1, 2), (1, 2, 0), (2, 0, 1)]]:
+                improper_idxs.append(canonicalize_bond((center, p[0], p[1], p[2])))
+                param_idxs.append(p_idx)
+
+        param_idxs = np.array(param_idxs)
+
+        # if no matches found, return arrays that can still be concatenated as expected
+        if len(param_idxs) > 0:
+            assigned_params = params[param_idxs]
+            improper_idxs = np.array(improper_idxs, dtype=np.int32)
         else:
-            # improper torsions do not use a valence dict as
-            # we cannot sort based on b_idxs[0] and b_idxs[-1]
-            # and reverse if needed. Impropers are centered around
-            # the first atom.
-            impropers = dict()
-
-            def make_key(idxs):
-                assert len(idxs) == 4
-                # pivot around the center
-                ctr = idxs[1]
-                # sort the neighbors so they're unique
-                nbs = idxs[0], idxs[2], idxs[3]
-                nbs = sorted(nbs)
-                return nbs[0], ctr, nbs[1], nbs[2]
-
-            for p_idx, patt in enumerate(smirks):
-                matches = match_smirks(mol, patt)
-
-                for m in matches:
-                    key = make_key(m)
-                    impropers[key] = p_idx
-
-            improper_idxs = []
-            param_idxs = []
-
-            for atom_idxs, p_idx in impropers.items():
-                center = atom_idxs[1]
-                others = [atom_idxs[0], atom_idxs[2], atom_idxs[3]]
-                for p in [(others[i], others[j], others[k]) for (i, j, k) in [(0, 1, 2), (1, 2, 0), (2, 0, 1)]]:
-                    improper_idxs.append(canonicalize_bond((center, p[0], p[1], p[2])))
-                    param_idxs.append(p_idx)
-
-            param_idxs = np.array(param_idxs)
-
-            # if no matches found, return arrays that can still be concatenated as expected
-            if len(param_idxs) > 0:
-                assigned_params = params[param_idxs]
-                improper_idxs = np.array(improper_idxs, dtype=np.int32)
-            else:
-                assigned_params = params[:0]  # empty slice with same dtype, other dimensions
-                improper_idxs = np.zeros((0, 4), dtype=np.int32)
-
+            assigned_params = params[:0]  # empty slice with same dtype, other dimensions
+            improper_idxs = np.zeros((0, 4), dtype=np.int32)
+    
         return assigned_params, improper_idxs
